1c6d6eb72f23af0fefe374b63171bdd205a25302
dawnlunacy <rudd.lacy@gmail.com>
2019-10-02 00:43:44 -0600

    Add search bar for layout purposes

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: ""
+        };
+    }
+        handleChange = event => {
+            this.setState({ [event.target.name]: event.target.value })
+        };
+
+        searchNews = event => {
+            event.preventDefault();
+
+            const newsTopicToSearch = {
+                topic: this.props.menuOption,
+                keyword: this.state.input
+            }
+
+            this.props.searchNewsFor(newsTopicToSearch)
+
+            this.setState({input: ""})
+        };
+
+    render() {
+        return (
+            <form>
+                <input
+                type="text"
+                placeholder="Search for news here"
+                name="search"
+                value={this.state.input}
+                onChange={event => this.handleChange(event)}
+                />
+                <button onClick ={event => this.searchNews(event)}>Search Now</button>
+            </form>
+        )
+    };
+};
 
 export default SearchForm;
