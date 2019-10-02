7b9358f34726078bd86fde00333cd1043cb6068e
dawnlunacy <rudd.lacy@gmail.com>
2019-10-01 23:58:41 -0600

    Add css styling and menu to append dynamically the buttons and uppercase the first letter for each

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './Menu.css';
 
 const Menu = (props) => {
-    console.log(props)
+    console.log(props.menu)
+    const menuOptions = Object.keys(props.menu);
+    const menuOption = menuOptions.map(option => <li key={option}><button className={[option,"menuItem"].join(' ')} > {option} </button></li>)
+    console.log(menuOption)
 return (
- <p> Hi </p>
+    <nav> 
+        <h1>What's New</h1> 
+        <ul>
+            {menuOption}
+        </ul>
+    </nav>
 )
 }
 
 export default Menu;
+
+
